03be9d9ead4f6805853b831e0c632f2dcfa6d870
Gabriel-2A <gaa.021205@gmail.com>
2022-05-09 11:51:31 -0500

    ...

---
+++ src/Circulo.java
@@ -0,0 +1,31 @@
+public class Circulo extends Figura{
+
+    private double radio;
+
+    public Circulo(String color, boolean relleno, double radio) {
+        super(color, relleno);
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double obtenerArea() {
+
+
+
+        return 0;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Todo Clase padre Figura, sera abstracta
+        //Atributos: color String, relleno boolenao
+        //constructor: para todos los parametros
+        //GETTERS Y SETTERS
+        //obtener area: abstracto y que retorne double
+        //obtener perimetro: abstracto y que retorne double
+
+
+        //todo clase circulo
+        //Radio double
+        //constructor: para todos los parametros y super
+        //GETTERS Y SETTERS
+        // implementar obtener area
+        // implementar obtener perimetro
+
+        //todo clase rectangulo
+        //ancho double
+        //alto double
+        //constructor: para todos los parametros y super
+        //GETTERS Y SETTERS
+        // implementar obtener area
+        // implementar obtener perimetro
+    }
+
+}
